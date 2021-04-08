ac71e6fb8cfe49ac1711aba46b737d3a508964e1
WrathTitan <rishabhbhatt159@gmail.com>
2021-04-08 12:24:35 +0530

    Model Performance Page and DB Add

--- app.py
+++ app.py
@@ -1,15 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
 import requests
-import random
 
 app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///testdb.sqlite3'
+db=SQLAlchemy(app)
+
+class MLModels(db.Model):
+	serialNumber=db.Column(db.Integer,primary_key=True)
+	modelname=db.Column(db.String(150),nullable=False)
+	modelparams=db.Column(db.String(300),nullable=True)
+	modelmetrics=db.Column(db.String(300),nullable=True)
+	def __init__(self,serialNumber,modelname,modelparams,modelmetrics):
+		self.serialNumber=serialNumber
+		self.modelname=modelname
+		self.modelparams=modelparams
+		self.modelmetrics=modelmetrics
+		
+
+db.create_all()
 
 
 @app.route("/")
 def myfunction():
-	return render_template("index.html",myvariable=myvariable)
+	myMLobject=MLModels(1,"KNN","Params","Metrics")
+	return render_template("index.html",myMLobject=myMLobject)
 
 
 if __name__=='__main__':
 	app.run(debug=True)
-	
